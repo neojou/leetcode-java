d7158bef3151d0da383eafc4c5044601ea0f6b86
neojou <neojou@gmail.com>
2022-12-13 23:47:25 +0800

    Take off dummy code
    Signed-off-by: neojou <neojou@gmail.com>

--- solution/src/com/nj/algo/search/NJBinarySearch.java
+++ solution/src/com/nj/algo/search/NJBinarySearch.java
@@ -1,25 +1,6 @@
 package com.nj.algo.search;
 
 public class NJBinarySearch {
-    protected int searchInsertRange(final int[] nums, int left, int right, int target) {
-        if (left > right)
-            return right + 1;
-
-        if (left == right) {
-            if (target > nums[right])
-                return right + 1;
-            return right;
-        }
-
-        int mid = (left + right)/2;
-        int bound = nums[mid];
-        if (target == bound)
-            return mid;
-        else if (target < bound)
-            return searchInsertRange(nums, left, mid - 1, target);
-        return searchInsertRange(nums, mid + 1, right, target);
-    }
-
     protected int searchSmallerRange(final int[] nums, int left, int right, int target) {
         if (left >= right)
             return left;
